18c29bc40c1c3da7da95263651e01c5fe5d61563
Kevin Schoen <kevschoen83@gmail.com>
2022-05-04 08:00:12 -0500

    Added week 5 coding assessment project

---
+++ fizz-buzz-bazz-coding-assessment/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+/*  Fizz Buzz Bazz (5 minutes)
+
+    Create a new Java Project with a Main method.
+        Create a method that will take in an Integer as a parameter.
+        In your method you need to check to see if the integer is divisible by 3 and
+        if it is then you will need to return fizz, if it's divisible by 5 then return buzz,
+        and lastly if it's divisible by 3 and 5 return fizz buzz bazz.
+        Call the method you created from main.
+        Print  your result to the console from main. */
